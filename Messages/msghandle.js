1401467a27b5168d74daf1e2c3a7ddbd559bf827
Pritt <pritt1999@gmail.com>
2018-08-31 09:37:04 +0530

    Added embed to voice command response

---
+++ Messages/msghandle.js
@@ -0,0 +1,226 @@
+//MongoDB
+
+const MongoClient = require('mongodb').MongoClient;
+var db;
+var cc;
+var reactions;
+
+MongoClient.connect('mongodb://userp:passp1@ds020218.mlab.com:20218/zilla-bot-gamma', { useNewUrlParser: true }, (err, client) => {
+    if(err) console.log(err);
+    console.log('Connected to database!');
+    db = client.db('zilla-bot-gamma');
+
+    db.collection('cc').find().toArray((err, results) => {
+        if(err) console.log(err);
+        cc = results[0];
+    });
+
+    db.collection('reactions').find().toArray((err, results) => {
+        if(err) console.log(err);
+        reactions = results[0];
+    });
+});
+
+//Google
+
+const google = require('google');
+
+const zbgmusic = '<:zbgmusic:484702955568758794>';
+
+//ClashOfClans API
+
+//const clashApi = require('clash-of-clans-api');
+const coctoken = require('./CocData/coctoken.json');
+
+//const client = clashApi({
+//    token: coctoken.value
+//});
+
+const zbgsword = '<:zbgsword:484289482640850945>';
+const zbgswordbroken = '<:zbgswordbroken:484292615999717376>';
+const zbgshield = '<:zbgshield:484296508775071744>';
+const zbgshieldbroken = '<:zbgshieldbroken:484296563418464256>';
+const zbgcocstarempty = '<:zbgcocstarempty:484275771784036354>';
+const zbgcocstarfull = '<:zbgcocstarfull:484275745728888832>';
+const zbgcocstarnew = '';
+
+//Discord Message Functions
+
+exports.responseMessage = function(message) {
+    if(message.content.startsWith('Z!')) {
+        var command = message.content.slice(2, message.content.length);
+       
+        for(var i=0; i<cc.list.length; i++) {
+            if(command.toLowerCase() == cc.list[i].name) {
+                return {
+                    text: cc.list[i].value
+                };
+                break;
+            }
+        }
+
+        if(command.startsWith('create-cc')) {
+            if(message.author.id == '334911278298562561') {
+                var namevalue = command.slice(10, command.length);
+                var spaceloc = namevalue.indexOf(" ");
+                var nname = namevalue.slice(0, spaceloc).toLowerCase();
+                var nvalue = namevalue.slice(spaceloc+1, namevalue.length);
+            
+                cc.list.push({
+                    name: nname,
+                    value: nvalue
+                });
+    
+                db.collection('cc').save(cc, (err, result) => {
+                    if(err) {
+                        return {
+                            text: 'Command creation failed! An error occured, please check the console.'
+                        };
+                        console.log(err);
+                    }
+                    console.log('CC updated!');
+                });
+                return {
+                    text: '[gamma] Command successfully created! Typing `Z!' + nname + '` will output `' + nvalue + '` now!'
+                };
+            }
+    
+            else {
+                return {
+                    text: 'You do not have the permission to create custom commands!'
+                };
+            }
+        }
+
+        else if(command.startsWith('delete-cc')) {
+            if(message.author.id == '334911278298562561') {
+                var dname = command.slice(10, command.length);
+                for(var j=0; j<cc.list.length; j++) {
+                    if(dname == cc.list[j].name) {
+                        cc.list.splice(j, 1);
+    
+                        db.collection('cc').save(cc, (err, result) => {
+                            if(err) {
+                                return {
+                                    text: 'Command deletion failed! An error occured, please check the console.'
+                                };
+                                console.log(err);
+                            }
+                            console.log('CC deleted!');
+                        });
+                    }
+                }
+                return {
+                    text: '[gamma] Command successfully deleted!'
+                };
+            }
+    
+            else {
+                return {
+                    text: 'You do not have the permissions to delete custom commands!'
+                };
+            }
+        }
+
+        else if(command == "list-cc") {
+            var plist = cc.list[0].name;
+            for(var k=1; k<cc.list.length; k++) {
+                plist = plist + ", " + cc.list[k].name;
+            }
+            return {
+                text: plist
+            };
+        }
+
+        else if(command.startsWith('reaction-add')) {
+            if(message.author.id == '334911278298562561') {
+                var namevalue = command.slice(13, command.length);
+                var spaceloc = namevalue.indexOf(" ");
+                var nname = namevalue.slice(0, spaceloc).toLowerCase();
+                var nvalue = namevalue.slice(spaceloc+1, namevalue.length);
+            
+                reactions.list.push({
+                    trigger: nname,
+                    output: nvalue
+                });
+    
+                db.collection('reactions').save(reactions, (err, result) => {
+                    if(err) {
+                        return {
+                            text: 'Command creation failed! An error occured, please check the console.'
+                        };
+                        console.log(err);
+                    }
+                    console.log('Reactions updated!');
+                });
+                return {
+                    text: '[gamma] Reaction successfully added! Typing `' + nname + '` anywhere in the message will add the reaction: `' + nvalue + '` now!'
+                };
+            }
+    
+            else {
+                return {
+                    text: 'You do not have the permission to add custom reactions!'
+                };
+            }
+        }
+
+        else if(command.startsWith('reaction-remove')) {
+            if(message.author.id == '334911278298562561') {
+                var dname = command.slice(16, command.length);
+                for(var j=0; j<reactions.list.length; j++) {
+                    if(dname == reactions.list[j].trigger) {
+                        reactions.list.splice(j, 1);
+    
+                        db.collection('reactions').save(reactions, (err, result) => {
+                            if(err) {
+                                return {
+                                    text: 'Reaction deletion failed! An error occured, please check the console.'
+                                };
+                                console.log(err);
+                            }
+                            console.log('Reaction deleted!');
+                        });
+                    }
+                }
+
+                return {
+                    text: '[gamma] Reaction successfully removed!'
+                };
+            }
+    
+            else {
+                return {
+                    text: 'You do not have the permissions to remove custom reactions!'
+                };
+            }
+        }
+    }
+    else if(message.content.startsWith('Zc!')) {
+        //Zilla Fam Clan commands
+    }
+    else {
+
+    }
+
+    return {
+        text: 'None'
+    };
+}
+
+exports.reactEmoji = function(messageContent) {
+    var reactEmojicontents = [];
+    var j=0;
+    for(var i=0; i<reactions.list.length; i++) {
+        if(messageContent.toLowerCase().indexOf(reactions.list[i].trigger) >= 0) {
+            reactEmojicontents[j] = reactions.list[i].output;
+            j++;
+        }
+    }
+
+    if(reactEmojicontents[0]) {
+        return reactEmojicontents;
+    }
+
+    return 'None';
+}
---
+++ main.js
@@ -0,0 +1,128 @@
+//HTTP-Express-BodyParser-Path
+var http = require('http');
+
+setInterval(() => {
+    //http.get('http://zillabotgamma.herokuapp.com/');
+    console.log('Pinging app');
+}, 1000*60*10);
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+
+const app = express();
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.listen(8888, () => {
+    console.log('Listening on Port 8888');
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+app.post('/applied', (req, res) => {
+    console.log(req.ip);
+});
+
+//Google and YTDL-Core
+
+const google = require('google');
+const ytdl = require('ytdl-core');
+
+//Discord
+
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+const token = require('./Confidential/token.json');
+
+var responseMessage = require('./Messages/msghandle.js').responseMessage;
+var reactEmoji = require('./Messages/msghandle.js').reactEmoji;
+
+var logzilla;
+var musiczilla;
+
+bot.on('ready', () => {
+    console.log('I am ready to be OP!');
+    bot.user.setActivity('the OPness radiation', {type: 'STREAMING', url: 'https://www.twitch.tv/prittclash'});
+    logzilla = bot.channels.get('409928557469630464');
+    musiczilla = bot.channels.get('392990490317946882');
+    musiczilla.leave();
+});
+
+bot.on('message', (message) => {
+    var sentmessage = responseMessage(message);
+
+    if(sentmessage.text != 'None') {
+        message.channel.send(sentmessage.text);
+    }
+
+    var isplaying = false;
+    if((message.content.startsWith('Z!play')) && (isplaying == false)) {
+        var query = message.content.slice(7, message.content.length);
+        var href;
+        var atext;
+        var ytid;
+        isplaying = true;
+
+        google(query + ' site:youtube.com', (err, res) => {
+            if(err) console.log(err);
+            
+            for(var i=0; i<res.links.length; i++) {
+                if(res.links[i].href != null) {
+                    href = res.links[i].href;
+                    ytid = href.slice(32, href.length);
+                    atext = `<:zbgmusic:484702955568758794> Now playing **` + res.links[i].title +`**`;
+                    break;
+                }
+            }
+
+            var streamOptions = {seek: 0, volume: 1};
+
+            musiczilla.join()
+            .then(connection => {
+                message.channel.send({embed: {
+                    color: 0xCBFDFC,
+                    description: atext,
+                    image: {
+                        url: `https://img.youtube.com/vi/${ytid}/hqdefault.jpg`
+                    }
+                }});
+                var stream = ytdl(href, { filter : 'audioonly' });
+                var dispatcher = connection.playStream(stream, streamOptions);
+                isplaying = true;
+                dispatcher.on('end', end => {
+                    setTimeout(() => {
+                        message.channel.send({embed: {
+                            color: 0xCBFDFC,
+                            description: '<:zbgmusic:484702955568758794> Song concluded. Leaving voice channel.'
+                        }});
+                        musiczilla.leave();
+                        isplaying = false;
+                    }, 500);
+                });
+            })
+            .catch(console.error);
+        });
+    }
+    else {
+        if(message.content.startsWith('Z!play')) {
+            message.channel.send('A song is already playing! Wait for it to end!');
+        }
+    }
+
+    if(message.content == 'Z!stop-playing') {
+        musiczilla.leave();
+        isplaying = false;
+    }
+
+    var reactedemoji = reactEmoji(message.content);
+    if((reactedemoji != 'None') && (message.author != bot.user)) {
+        for(var i=0; i<reactedemoji.length; i++) {
+            message.react(reactedemoji[i]);
+        }
+    }
+});
+
+bot.login(token.value);
